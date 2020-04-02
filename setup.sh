7bc87e598bc2d8c3eed4c12ac6c2cc0f7ecc7b57
intermezzio <amascillaro@gmail.com>
2020-04-02 17:00:11 -0400

    Update the setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 
 rm -rf stock-market-prediction
 git rm --cached stock-market-prediction
+rm .gitmodules
+touch .gitmodules
+git config -f .git/config --remove-section submodule.stock-market-prediction
 git submodule add https://github.com/intermezzio/stock-market-prediction
+cd ./stock-market-prediction
+sh setup.sh
+cd ..
 mkdir config
-echo "API_KEY = 'your-api-key'\nSECRET_KEY = 'your-secret-key'\nIEX_API_TOKEN = 'your-iex-token'" >> config/config.py
+printf "APCA_API_KEY_ID = 'your-api-key'\nAPCA_API_SECRET_KEY = 'your-secret-key'\nALPHAVANTAGE_API_KEY = 'your-alphavantage-token'\n\nAPCA_API_BASE_URL = 'https://paper-api.alpaca.markets'" >> config/config.py
